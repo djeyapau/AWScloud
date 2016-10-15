79365f6ba8f20cc68dd1a71864c377c70317bab0
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-15 14:33:07 +0000

    Second commit: Made initial positional parameter changes. Remove runtime input everything except client token

--- create-env.sh
+++ create-env.sh
@@ -2,47 +2,32 @@
 
 echo "\nWelcome to Dharshini's first load balancer\n"
 
-#input image id
-echo "Enter the image id: "
-read imgid
-echo "\n"
-
 #input client token
 echo "Enter a name for the group of instances (client token): "
 echo "(Hint: Do not repeat any whole or part of an already used client token)"
 read ct
 echo "\n"
 
-#input key name
-echo "Enter the key name: "
-read keyname
-echo "\n"
-
-#input security group id
-echo "Enter the security group id: "
-read secgp
-echo "\n"
-
 #run 3 instances
-aws ec2 run-instances --image-id $imgid --key-name $keyname --security-group-ids $secgp --instance-type t2.micro --count 3 --client-token $ct --user-data file://installapp.sh --placement AvailabilityZone=us-west-2b
+aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --instance-type t2.micro --count $5 --client-token $ct --user-data file://installapp.sh --placement AvailabilityZone=us-west-2b
 
 #store the instances IDs into a variable
-inst=$(aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId,ClientToken]' | grep $ct | awk '{print $1}')
+inst=`aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId,ClientToken]' | grep $ct | awk '{print $1}'`
 
 #wait till the instances run
 aws ec2 wait instance-running --instance-ids $inst
 
 #create load balancer
-aws elb create-load-balancer --load-balancer-name djlb --listeners "Protocol=Http, LoadBalancerPort=80, InstanceProtocol=Http, InstancePort=80" --availability-zones us-west-2b --security-groups $secgp
+aws elb create-load-balancer --load-balancer-name djlb --listeners "Protocol=Http, LoadBalancerPort=80, InstanceProtocol=Http, InstancePort=80" --availability-zones us-west-2b --security-groups $3
 
 #register the instances with the load balancer
 aws elb register-instances-with-load-balancer --load-balancer-name djlb --instances $inst
 
 #create launch configuration
-aws autoscaling create-launch-configuration --launch-configuration-name djlc --image-id $imgid --key-name $keyname --instance-type t2.micro --user-data file:/installapp.sh
+aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --key-name $2 --instance-type t2.micro --user-data file:/installapp.sh
 
 #create auto scaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name djasg --launch-configuration djlc --availability-zone us-west-2b --max-size 5 --min-size 0 --desired-capacity 1
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name djasg --launch-configuration $4 --availability-zone us-west-2b --max-size 5 --min-size 0 --desired-capacity 1
 
 #attach instances to auto scaling group
 aws autoscaling attach-instances --instance-ids $inst --auto-scaling-group-name djasg
@@ -50,6 +35,6 @@ aws autoscaling attach-instances --instance-ids $inst --auto-scaling-group-name 
 #attach load balancer to auto scaling group
 aws autoscaling attach-load-balancers --load-balancer-names djlb --auto-scaling-group-name djasg
 
-aws autoscaling update-auto-scaling-group --auto-scaling-group-name djasg --launch-configuration-name djlc --min-size 2
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name djasg --launch-configuration-name $4 --min-size 2
 
 echo "The script completed"
